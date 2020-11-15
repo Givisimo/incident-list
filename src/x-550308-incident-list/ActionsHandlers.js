1f55602aad370298cfebddd83414317e09c9a7bf
Denys Plietnikov <givisimo@gmail.com>
2020-11-15 18:39:27 +0200

    constants rename

--- src/x-550308-incident-list/ActionsHandlers.js
+++ src/x-550308-incident-list/ActionsHandlers.js
@@ -1,15 +1,19 @@
 import { actionTypes } from '@servicenow/ui-core';
-import { INCIDENT_FETCH_SUCCESS, FETCH_LATEST_INCIDENT } from './constants';
+import {
+  INCIDENT_FETCH_SUCCEEDED,
+  FETCH_LATEST_INCIDENT_STARTED,
+} from './constants';
 import { getIncidents } from './getHttpEffect';
 
 const { COMPONENT_BOOTSTRAPPED } = actionTypes;
 
 export const actionHandlers = {
-  [COMPONENT_BOOTSTRAPPED]: ({ dispatch }) => dispatch(FETCH_LATEST_INCIDENT),
+  [COMPONENT_BOOTSTRAPPED]: ({ dispatch }) =>
+    dispatch(FETCH_LATEST_INCIDENT_STARTED),
 
-  [FETCH_LATEST_INCIDENT]: getIncidents,
+  [FETCH_LATEST_INCIDENT_STARTED]: getIncidents,
 
-  [INCIDENT_FETCH_SUCCESS]: ({ action, updateState }) => {
+  [INCIDENT_FETCH_SUCCEEDED]: ({ action, updateState }) => {
     const { result } = action.payload;
     updateState({ incidents: result });
   },
--- src/x-550308-incident-list/getHttpEffect.js
+++ src/x-550308-incident-list/getHttpEffect.js
@@ -1,7 +1,7 @@
 import { createHttpEffect } from '@servicenow/ui-effect-http';
-import { URL_INCIDENT_TABLE, INCIDENT_FETCH_SUCCESS } from './constants';
+import { URL_INCIDENT_TABLE, INCIDENT_FETCH_SUCCEEDED } from './constants';
 
 export const getIncidents = createHttpEffect(URL_INCIDENT_TABLE, {
   method: 'GET',
-  successActionType: INCIDENT_FETCH_SUCCESS,
+  successActionType: INCIDENT_FETCH_SUCCEEDED,
 });
